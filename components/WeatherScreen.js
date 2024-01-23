c58cb959df5ae0f8186e7ac3c253a42d2720c7b9
jake-knox- <jakeknox11@hotmail.co.uk>
2024-01-23 10:20:30 +0000

    files and config setup

---
+++ components/WeatherScreen.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import axios from 'axios';
+
+import config from './config';
+
+const WeatherScreen = () => {
+    const apiKey = config.apiKey;
+
+    return (
+        <View>
+
+        </View>
+    );
+}
+export default WeatherScreen;
